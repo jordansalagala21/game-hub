4f519838d1e09cc96b5b699af720227350addedc
Jordan Salagala <officjordansal@gmail.com>
2024-09-16 20:49:49 -0400

    Fetch the Genres

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,8 +2,8 @@ import { Card, Skeleton, CardBody, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width="300px" borderRadius={10} overflow="hidden">
-      <Skeleton height="200px" />
+    <Card>
+      <Skeleton />
       <CardBody>
         <SkeletonText />
       </CardBody>
